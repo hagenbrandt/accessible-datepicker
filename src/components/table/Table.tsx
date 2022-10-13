2f6feae1d118946059b9e32b5c42c3d516e5a5e3
hbrandt <h.brandt@steinberg.de>
2022-10-13 10:50:46 +0200

    feat: Renders a cell in table body and add titles to rows

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 type TableType = {
   head: string;
+  headerRowTitle: string;
+  bodyTitle: string;
+  bodyRowTitle: string;
+  cell: string;
 };
 
-export const Table = ({ head }: TableType) => {
+export const Table = ({ head, headerRowTitle, bodyTitle, bodyRowTitle, cell }: TableType) => {
   return (
     <table>
-      <thead>
-        <tr>
+      <thead title={headerRowTitle}>
+        <tr title={headerRowTitle}>
           <th>{head}</th>
         </tr>
       </thead>
+      <tbody title={bodyTitle}>
+        <tr title={bodyRowTitle}>
+          <td>{cell}</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
--- src/components/table/__tests__/Table.test.tsx
+++ src/components/table/__tests__/Table.test.tsx
@@ -4,7 +4,19 @@ import { Table } from '../Table';
 
 describe('table', () => {
   const head = 'Head 1';
-  const component = <Table head={head} />;
+  const headerRowTitle = 'Header';
+  const bodyTitle = 'Body Title';
+  const bodyRowTitle = 'Body Row 1';
+  const cell = 'Table Element 1';
+  const component = (
+    <Table
+      head={head}
+      headerRowTitle={headerRowTitle}
+      bodyTitle={bodyTitle}
+      bodyRowTitle={bodyRowTitle}
+      cell={cell}
+    />
+  );
 
   beforeEach(() => {
     render(component);
@@ -15,8 +27,14 @@ describe('table', () => {
   });
 
   it('renders a columnheader with a given string in a rowgroup', () => {
-    expect(screen.getByRole('rowgroup')).toBeInTheDocument();
+    expect(screen.getByRole('rowgroup', { name: headerRowTitle })).toBeInTheDocument();
     expect(screen.getByRole('columnheader')).toHaveTextContent(head);
-    expect(screen.getByRole('row')).toHaveTextContent(head);
+    expect(screen.getByRole('row', { name: headerRowTitle })).toHaveTextContent(head);
+  });
+
+  it('renders a cell within a row and a tablebody', () => {
+    expect(screen.getByRole('rowgroup', { name: bodyTitle })).toBeInTheDocument();
+    expect(screen.getByRole('row', { name: bodyRowTitle })).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: cell })).toBeInTheDocument();
   });
 });
